9100050f2041237e4cf310dc737abbaa593a2eb6
Jason Wang <qwang216@gmail.com>
2015-06-27 15:38:33 -0400

    new

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,29 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Item : NSObject {
-    NSString *xyz;
-}
 
-@property (nonatomic) NSString *itemContent;
-@property (nonatomic) int *itemPriority;
--(void)setXYZ:(NSString *)value;
--(NSString *)xyz;
+
+@interface Item : NSObject
+
+@property (nonatomic) NSString *content;
+-(NSString *)inputContentItem;
 @end
 
-//@interface ManageItem : NSObject
-//@property (nonatomic) NSString *markItem;
-//@property (nonatomic) int priorityItem;
-//@property (nonatomic) BOOL markDoneItem;
-//@property (nonatomic) NSString *addItem;
-//@property (nonatomic) NSString *deletItem;
-//
-//@end
+
+
+
+
 
 @implementation Item
-
-- (void)setXYZ:(NSString *)value {
-    _xyx = value;
+-(NSString *)inputContentItem {
+    scanf(@"%@", intputContentItem);
 }
 
 
@@ -40,9 +33,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // START CODE HERE
-        
-        Item *aItem = [Item new];
-    aItem
+
         
         // END CODE HERE
     }
