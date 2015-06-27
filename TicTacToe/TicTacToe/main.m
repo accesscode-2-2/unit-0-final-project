65eca63106883f96b4c4239c683b0c931004d593
Jovanny Espinal <espinaljovanny@icloud.com>
2015-06-27 14:16:31 -0400

    Display board class test

--- TicTacToe/TicTacToe/main.m
+++ TicTacToe/TicTacToe/main.m
@@ -8,38 +8,47 @@
 
 #import <Foundation/Foundation.h>
 
-@
+@interface Board : NSObject
+
+-(void)displayBoard;
+
+@end
+
+@implementation Board
+
+-(void)displayBoard{
+    NSMutableArray *board = [[NSMutableArray alloc] init];
+    [board insertObject:[NSMutableArray arrayWithObjects:@"1", @"2", @"3", nil] atIndex:0];
+    [board insertObject:[NSMutableArray arrayWithObjects:@"4", @"5", @"6", nil] atIndex:1];
+    [board insertObject:[NSMutableArray arrayWithObjects:@"7", @"8", @"9", nil] atIndex:2];
+    
+    for (int i = 0; i < 3 ; i++) {
+        int j = 0;
+        NSString *rowColumn = [NSString stringWithFormat:@"%@ | %@ | %@", board[i][j], board[i][j+1], board[i][j+2]];
+        NSLog(@"%@", rowColumn);
+        
+        if (i < 2){
+            NSLog(@"---------");
+        }
+        
+    }
+    
+}
+
+@end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        NSMutableArray *board = [[NSMutableArray alloc] init];
-        [board insertObject:[NSMutableArray arrayWithObjects:@"1", @"2", @"3", nil] atIndex:0];
-        [board insertObject:[NSMutableArray arrayWithObjects:@"4", @"5", @"6", nil] atIndex:1];
-        [board insertObject:[NSMutableArray arrayWithObjects:@"7", @"8", @"9", nil] atIndex:2];
-        
-        NSString *decision = @"X";
-        board[0][0] = decision;
-    
-        
-        
-            for (int i = 0; i < 3 ; i++) {
-                int j = 0;
-                NSString *rowColumn = [NSString stringWithFormat:@"%@ | %@ | %@", board[i][j], board[i][j+1], board[i][j+2]];
-                NSLog(@"%@", rowColumn);
-                
-                if (i < 2){
-                NSLog(@"---------");
-                }
-    
-                
-            }
-        
-        
-        
-
-        
-        
-        return 0;
-    }
-}
+        Board *normal = [[Board alloc] init];
+                         [normal displayBoard];
+                         
+                         
+                         
+                         
+                         
+                         
+                         
+                         return 0;
+                         }
+                         }
