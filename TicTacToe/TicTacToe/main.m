de00124caad7d1be0ce7e798245b9eab0c5e0142
Henna <henna.ahmed92@gmail.com>
2015-06-27 15:22:59 -0400

    implemented computer choice and printing board

--- TicTacToe/TicTacToe/main.m
+++ TicTacToe/TicTacToe/main.m
@@ -11,6 +11,7 @@
 
 -(void) initializeArray: (int) dimension;
 -(void) printBoard;
+-(BOOL) isPositionValidX: (int) x AndY: (int) y and: (char) userPosition;
 
 @end
 
@@ -23,11 +24,9 @@
     _board = [[NSMutableArray alloc] init];
     
     for (int i = 0; i<dimension; i++) {
-        NSArray * row = [NSArray arrayWithObjects:@" ", @" ", @" ", nil];
+        NSMutableArray * row = [NSMutableArray arrayWithObjects:@" ", @" ", @" ", nil];
         [_board addObject:row];
     }
-
-//    NSLog(@"%@", _board);
 }
 
 -(void)printBoard {
@@ -37,6 +36,32 @@
     NSArray *row3 = [_board objectAtIndex:2];
     
     NSLog(@"\n\t\t|%@|%@|%@| \n\t\t|%@|%@|%@| \n\t\t|%@|%@|%@|", [row1 objectAtIndex:0], [row1 objectAtIndex:1], [row1 objectAtIndex:2], [row2 objectAtIndex:0], [row2 objectAtIndex:1], [row2 objectAtIndex:2], [row3 objectAtIndex:0], [row3 objectAtIndex:1], [row3 objectAtIndex:2]);
+}
+
+-(BOOL) isPositionValidX: (int) x AndY: (int) y and: (char) userPosition {
+    
+    x = x-1;    // this is the position on the row-array we want to check
+    y = y-1;    // this is the position in our board array we want the row-array from
+    
+    if ([[[_board objectAtIndex:y] objectAtIndex:x] isEqualTo:@" "]) { // get row-array from board, get element from row-array and then compare
+        if (userPosition == 'X') {
+            
+            [[_board objectAtIndex:y] replaceObjectAtIndex:x withObject:@"X"];
+            
+        }
+        else{
+            [[_board objectAtIndex:y] replaceObjectAtIndex:x withObject:@"O"];
+        }
+        return YES;
+    }
+    else {
+        NSLog(@"This is a not valid position");
+        return NO;
+    }
+    
+    
+    
+    
 }
 
 
@@ -51,66 +76,97 @@ int main(int argc, const char * argv[]) {
         ticTacToeManager * game = [[ticTacToeManager alloc] init];
         
         [game initializeArray:3];
-        [game printBoard];
         
-        char userXOrO;
-        char otherUserXOrO;
+        
+        char xOrO;
         
         while (true) {
             
             NSLog(@"Choose X or O: ");
             fpurge(stdin);
-            scanf("%c", &userXOrO);
+            scanf("%c", &xOrO);
             
-            if ( (userXOrO == 'x') || (userXOrO == 'X') || (userXOrO == 'o') || (userXOrO == 'O') ) {
+            if ( (xOrO == 'x') || (xOrO == 'X') || (xOrO == 'o') || (xOrO == 'O') ) {
                 
-                if (userXOrO == 'x' || userXOrO =='X') {
-                    userXOrO = 'X';
-                    otherUserXOrO = 'O';
+                if (xOrO == 'x' || xOrO =='X') {
+                    xOrO = 'X';
                 }
                 else {
-                    userXOrO = 'O';
-                    otherUserXOrO = 'X';
+                    xOrO = 'O';
                 }
                 
                 break;
             }
         }
         
+        BOOL computerTurn = NO;
+        
         while (true) {
             
             int hPosition = -1;
             int vPosition = -1;
-//            NSInteger *hPosition;
-//            NSInteger *vPosition;
-            NSLog(@"Choose your horizontal position (1-3): ");
-            fpurge(stdin);
-            scanf("%d", &hPosition);
+            
+            
+            if (computerTurn) {
 
-            if (hPosition < 1 || hPosition > 3) {
-                NSLog(@"Please enter a valid position between 1 and 3");
-                continue;
+                hPosition = arc4random_uniform(2) + 1;
+                vPosition = arc4random_uniform(2) + 1;
+                
+                //pick a hposition and vposition for computer
             }
             
-            NSLog(@"Choose your vertical position (1-3): ");
-            fpurge(stdin);
-            scanf("%d", &vPosition);
-            
-            if (vPosition < 1 || vPosition > 3) {
-                NSLog(@"Please enter a valid position betwwwn 1 and 3");
+            else{
+
+                NSLog(@"Choose your horizontal position (1-3): ");
+                fpurge(stdin);
+                scanf("%d", &hPosition);
+                
+                if (hPosition < 1 || hPosition > 3) {
+                    NSLog(@"Please enter a valid position between 1 and 3");
+                    continue;
+                }
+                
+                NSLog(@"Choose your vertical position (1-3): ");
+                fpurge(stdin);
+                scanf("%d", &vPosition);
+                
+                if (vPosition < 1 || vPosition > 3) {
+                    NSLog(@"Please enter a valid position betwwwn 1 and 3");
+                }
             }
             
-            NSLog(@"%d %d", hPosition, vPosition);
             
             // Pass in this position into our class and then check if it's empty and if so place this person's x or o into the board
             
-            break;
+            if ([game isPositionValidX:hPosition AndY:vPosition and:xOrO]){
+                if (computerTurn == YES) {
+                    computerTurn = NO;
+                }
+                else{
+                    computerTurn = YES;
+                }
+                if (xOrO == 'X') {  //After each iteration we want to change the postion to X or back to O
+                    xOrO = 'O';
+                }
+                else{
+                    xOrO = 'X';
+                }
+                
+            }
+            [game printBoard];
+            
+            
+            
+            
+            
+            
+            //break;
             
             
             
         }
         
-
+        
     }
     return 0;
 }
