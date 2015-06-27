090d93321cb38c2ebae0aa666426da98e83bbe01
Christian <maldonadoguitar@gmail.com>
2015-06-27 12:54:47 -0400

    justine's turn

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,10 +8,97 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Manager : NSObject
+-(void)addItem;
+-(void)deleteItem;
+-(void)markDone;
+-(NSMutableArray)setPriority:(NSInteger *)i;
+
+
+@end
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+@implementation Manager;
+
+
+
+@end
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+@interface List : NSObject
+
+
+-(void)setListName:(NSString *)list;
+-(NSString *) list;
+-(void)addItem:(Item *)item;
+
+@end
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+@implementation List{
+    NSString * _list;
+    NSMutableArray* _listOfItems;
+}
+
+
+-(void)setListName:(NSString *)list{
+    _list = list;
+}
+-(NSString *) list{
+    return _list;
+}
+-(void)addItem:(Item *)item{
+    if(_listOfItems == nil){
+        _listOfItems = [[NSMutableArray alloc] init];
+    }
+    [_listOfItems addObject:item];
+}
+
+
+@end
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+@interface Item : NSObject
+
+@end
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+@implementation Item{
+    
+    NSString * _item;
+}
+
+
+-(void)setItemName:(NSString *)item{
+    _item = item;
+}
+-(NSString *) item{
+    return _item;
+}
+
+
+
+
+
+
+@end
+
+
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
     }
     return 0;
 }
+
+
+@end
