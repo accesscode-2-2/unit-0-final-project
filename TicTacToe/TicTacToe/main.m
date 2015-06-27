51173e26d8c6b67ddd7d4ed15371aa98859ee3be
Henna <henna.ahmed92@gmail.com>
2015-06-27 12:18:55 -0400

    initialized board and printed it

--- TicTacToe/TicTacToe/main.m
+++ TicTacToe/TicTacToe/main.m
@@ -2,16 +2,57 @@
 //  main.m
 //  TicTacToe
 //
-//  Created by Michael Kavouras on 6/25/15.
-//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//  Created by Henna and Kaisha  on 6/27/15.
 //
 
 #import <Foundation/Foundation.h>
 
+@interface ticTacToeManager : NSObject
+
+-(void) initializeArray: (int) dimension;
+-(void) printBoard;
+
+@end
+
+@implementation ticTacToeManager{
+    NSMutableArray *_board;
+}
+
+-(void) initializeArray: (int) dimension{
+    
+    _board = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i<dimension; i++) {
+        NSArray * row = [NSArray arrayWithObjects:@" ", @" ", @" ", nil];
+        [_board addObject:row];
+    }
+
+//    NSLog(@"%@", _board);
+}
+
+-(void)printBoard {
+    
+    NSArray *row1 = [_board objectAtIndex:0];
+    NSArray *row2 = [_board objectAtIndex:1];
+    NSArray *row3 = [_board objectAtIndex:2];
+    
+    NSLog(@"\n\t\t|%@|%@|%@| \n\t\t|%@|%@|%@| \n\t\t|%@|%@|%@|", [row1 objectAtIndex:0], [row1 objectAtIndex:1], [row1 objectAtIndex:2], [row2 objectAtIndex:0], [row2 objectAtIndex:1], [row2 objectAtIndex:2], [row3 objectAtIndex:0], [row3 objectAtIndex:1], [row3 objectAtIndex:2]);
+}
+
+
+
+@end
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        ticTacToeManager * game = [[ticTacToeManager alloc] init];
+        
+        [game initializeArray:3];
+        [game printBoard];
+
     }
     return 0;
 }
