92a436116d0516c51581cc9f2130a5030840d153
Brian <brianblanco1@gmail.comm>
2015-06-27 14:24:54 -0400

    2nd update

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,10 +8,61 @@
 
 #import <Foundation/Foundation.h>
 
+@interface toDoList: NSObject
+
+-(void) enterUserList: (NSMutableArray * )userList;
+-(NSArray *)userList;
+-(void) manageList: (NSArray *)manList;
+-(NSArray *)manList;
+-(void) representList: (NSArray *)repList;
+-(NSArray *)repList;
+-(void) representItem: (NSString *)repItem;
+-(NSString *)repItem;
+
+@end
+
+@implementation toDoList {
+    NSMutableArray *_userList;
+    NSArray *_manList;
+    NSArray *_repList;
+    NSString *_repItem;
+}
+
+-(void) enterUserList: (NSMutableArray *)userList {
+    _userList = userList;
+}
+
+-(void) manageList:(NSArray *)manList {
+    _manList = manList;
+}
+
+-(void) representList: (NSArray *)repList {
+    _repList = repList;
+}
+
+-(void) representItem: (NSString *)repItem {
+    _repItem = repItem;
+}
+
+
+
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        toDoList *Charles = [[toDoList alloc]init];
+        NSMutableArray *charlesList = [[NSMutableArray alloc]init];
+        NSLog(@"Good Morning, what are you going to do today?");
+        
+        scanf(@"%@, %@, %@, %@\n", &item1, &item2, &item, &item4);
+        *charlesList = [Charles userList:[NSMutableArray arrayWithObjects:@"item1", @"item2", @"item3", @"item4", nil]];
+        
+        
+        
+        
+
     }
     return 0;
 }
