5099961e53deb70c78c3a93fdd2200cfd3fdac0b
Justine Gartner <GartnerJustine@gmail.com>
2015-06-27 15:40:08 -0400

    Adding items and displaying them

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,22 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Manager : NSObject
--(void)addItem;
--(void)deleteItem;
--(void)markDone;
--(NSMutableArray)setPriority:(NSInteger *)i;
+@class List;
+@class Item;
+@class Manager;
 
-
-@end
-
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-@implementation Manager;
-
-
-
-@end
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -31,74 +19,138 @@
 @interface List : NSObject
 
 
--(void)setListName:(NSString *)list;
--(NSString *) list;
+- (instancetype)initWithListName:(NSString *)listName;
+
 -(void)addItem:(Item *)item;
+
+-(NSMutableArray *)listOfItems;
+
+
 
 @end
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 @implementation List{
-    NSString * _list;
+    NSString* _list;
     NSMutableArray* _listOfItems;
 }
 
 
--(void)setListName:(NSString *)list{
-    _list = list;
+- (instancetype)initWithListName:(NSString *)listName{
+    if (self = [super init]) {
+        _list = listName;
+        return self;
+    }
+    return nil;
+    
 }
--(NSString *) list{
-    return _list;
-}
--(void)addItem:(Item *)item{
+
+-(NSMutableArray *)listOfItems{
     if(_listOfItems == nil){
         _listOfItems = [[NSMutableArray alloc] init];
     }
-    [_listOfItems addObject:item];
+    return _listOfItems;
 }
+
+-(void)addItem:(Item *)item{
+    [[self listOfItems] addObject:item];
+}
+
 
 
 @end
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+
 @interface Item : NSObject
+
+- (instancetype)initWithItemName:(NSString *)itemName;
 
 @end
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 @implementation Item{
+    NSString * _title;
+}
+
+
+- (instancetype)initWithItemName:(NSString *)itemName{
+    if (self = [super init]) {
+        _title = itemName;
+        return self;
+    }
+    return nil;
     
-    NSString * _item;
 }
 
+@end
 
--(void)setItemName:(NSString *)item{
-    _item = item;
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+@interface Manager : NSObject
+
+-(void)displayItems:(NSMutableArray *)items;
+
+//-(void)markDone;
+//-(void)setPriority:(NSInteger *)i;
+
+
+@end
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+@implementation Manager{
+    
 }
--(NSString *) item{
-    return _item;
+
+-(void)displayItems:(List *)list{
+    NSMutableArray *myListOfItems = [list listOfItems];
+    for (int i = 0; i < [myListOfItems count]; i++) {
+        NSLog(@"%@", myListOfItems[i]);
+    }
 }
-
-
-
 
 
 
 @end
 
-
-
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
+        Manager *listManagaer = [[Manager alloc] init];
+        
+        List *toDoToday = [[List alloc] initWithListName:@"To Do Today"];
+        
+        Item *swim = [[Item alloc] initWithItemName:@"swim"];
+        Item *yoga = [[Item alloc] initWithItemName:@"yoga"];
+        Item *practiceGuitar = [[Item alloc] initWithItemName:@"practice guitar"];
+        
+        [toDoToday addItem:swim];
+        [toDoToday addItem:yoga];
+        [toDoToday addItem:practiceGuitar];
+        
+        //NSLog(@"%@, %@, %@", toDoToday[0], toDoToday[1], toDoToday[2]);
+        
+        [listManagaer displayItems:toDoToday];
+        
+        
+        
+        
+        
         
     }
     return 0;
 }
 
 
-@end
+//@end
