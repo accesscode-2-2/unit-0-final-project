fc21d9ace1239bbd13b45cd44134d17721bd0820
Artur Lane <arturlan.us@gmail.com>
2015-06-27 14:19:28 -0400

    First commit

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,10 +8,66 @@
 
 #import <Foundation/Foundation.h>
 
+
+@interface ToDoList: NSObject
+
+@property  NSMutableArray *_taskArray;
+
+
+-(void)listItems:(NSMutableArray *)listItems;
+-(NSMutableArray *)listItems;
+
+
+//-(void)addItems:(NSMutableArray *)addItems;
+//-(NSString *)addItems;
+//
+//-(void)removeItems:(NSMutableArray *)removeItems;
+////removeItems;
+//
+//-(void)markItemDone:(NSMutableArray *)markItemDone;
+//-(NSMutableArray *)listItems;
+//
+//-(void)listCompletedItem:(NSMutableArray *)listCompletedItem;
+//-(NSMutableArray *)listCompletedItem;
+
+
+@end
+
+
+@implementation ToDoList
+
+-(void)listItems:(NSMutableArray *)listItems {
+    
+    for(int counter=0; counter<=[listItems count]; counter++){
+        NSLog(@"%c", listItems[counter].name);
+        
+    }
+    
+}
+@end
+
+////////////////////
+
+@interface Item: NSObject
+
+@end
+
+@implementation Item
+
+
+@end
+
+////
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+
+        
+        
     }
     return 0;
 }
