a4006294a78dac8ad0dca8b432d325a6518efa4c
Jovanny Espinal <espinaljovanny@icloud.com>
2015-06-28 15:48:28 -0400

    MVP

--- TicTacToe/TicTacToe/main.m
+++ TicTacToe/TicTacToe/main.m
@@ -63,6 +63,7 @@
     NSString *spaceChosen;
     NSString *playerChoice;
     NSString *symbolChoice;
+    int randomSelection;
     Player *player1;
     Player *player2;
     
@@ -72,49 +73,67 @@
 // Allows player to choose whether to play against human  or computer
 
 -(void)introScreen{
+    
+    //Initiate player objects
     player1 = [[Player alloc] init];
+    player2 = [[Player alloc] init];
+    
+    //Ask user for choice of playing vs human or cpu
     NSLog(@"Welcome to Tic-Tac-Toe! Will you be playing against a friend or the CPU?\nA. A Friend\nB. CPU");;
     char choice[256];
     scanf("%s", choice);
     playerChoice = [NSString stringWithCString:choice encoding:NSUTF8StringEncoding];
     
+    //Input validation
     while (!([playerChoice isEqualToString:@"A"] || [playerChoice isEqualToString: @"a"] || [playerChoice isEqualToString:@"B"] || [playerChoice isEqualToString: @"b"])){
         NSLog(@"Please choose from the options available.\nA. A Friend\nB. CPU");
         scanf("%s", choice);
         playerChoice = [NSString stringWithCString:choice encoding:NSUTF8StringEncoding];
     }
     
-    if ([playerChoice isEqualToString:@"A"] || [playerChoice isEqualToString: @"a"]) {
-        player2 = [[Player alloc] init];
-        NSLog(@"Player 1, what symbol would you like to use: X or O?");
+    //Ask user to select a symbol to use
+    NSLog(@"Player 1, what symbol would you like to use: X or O?");
+    scanf("%s", choice);
+    symbolChoice = [NSString stringWithCString:choice encoding:NSUTF8StringEncoding];
+    
+    //Input validation
+    while (!([symbolChoice isEqualToString:@"x"] || [symbolChoice isEqualToString:@"X"] || [symbolChoice isEqualToString:@"o"] || [symbolChoice isEqualToString:@"O"])) {
+        NSLog(@"Please choose from the options available: X or O?");
         scanf("%s", choice);
-        symbolChoice = [NSString stringWithCString:choice encoding:NSUTF8StringEncoding];
-        
-        while (!([symbolChoice isEqualToString:@"x"] || [symbolChoice isEqualToString:@"X"] || [symbolChoice isEqualToString:@"o"] || [symbolChoice isEqualToString:@"O"])) {
-            NSLog(@"Please choose from the options available: X or O?");
-            scanf("%s", choice);
-            symbolChoice =[NSString stringWithCString:choice encoding:NSUTF8StringEncoding];
-        }
-        
-        [player1 setSymbol:symbolChoice];
-        if ([[player1 getSymbol] isEqualToString:@"x"] || [[player1 getSymbol] isEqualToString:@"X"]) {
-            [player2 setSymbol:@"O"];
-        }
-        else {
-            [player2 setSymbol:@"X"];
-        }
+        symbolChoice =[NSString stringWithCString:choice encoding:NSUTF8StringEncoding];
+    }
+    
+    //Set player 1 symbol
+    [player1 setSymbol:symbolChoice];
+    
+    //Set player 2 symbol based on player 1's symbol
+    if ([[player1 getSymbol] isEqualToString:@"x"] || [[player1 getSymbol] isEqualToString:@"X"]) {
+        [player2 setSymbol:@"O"];
+    }
+    else {
+        [player2 setSymbol:@"X"];
     }
 }
 
+
+//Ask user1 for their space
 -(void)askUser1ForSpace{
     
+    
     BOOL invalid = YES;
+    
+    //Loop while player 1 selects a space that is already occupied
     while (invalid) {
-        NSLog(@"Player 1, please enter the space you'd like to occupy.");
+        
+        //Ask for input of space
+        NSLog(@"\n\nPlayer 1, please enter the space you'd like to occupy.\n\n");
         char space[256];
         scanf("%s", space);
+        
+        //Convert char character into NSString
         spaceChosen = [NSString stringWithCString:space encoding:NSUTF8StringEncoding];
         
+        //For loop that loops through the rows and columns of the board to see if that space is occupied; continue looping if so.
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++) {
                 if ([spaceChosen isEqualToString:_normalBoard[i][j]]) {
@@ -123,10 +142,17 @@
             }
         }
     }
+    
+    //Set player space to the space chosen if available
     [player1 setSpace:spaceChosen];
+    
+    //Loop through array; if space on board is equal to the player's space (which means it's available), replace that space with the player's symbol
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++) {
+            //Check if player space is available
             if ([[player1 getSpace] isEqualToString:_normalBoard[i][j]]) {
+                
+                //If space is available, replace the space with the player's symbol
                 if ([[player1 getSymbol] isEqualToString:@"X"] || [[player1 getSymbol] isEqualToString:@"x"]) {
                     _normalBoard[i][j] = @"X";
                 }
@@ -137,6 +163,7 @@
         }
     }
     
+    //Prints out board after the player's selection
     for (int i = 0; i < 3 ; i++) {
         int j = 0;
         NSString *rowColumn = [NSString stringWithFormat:@"%@ | %@ | %@", _normalBoard[i][j], _normalBoard[i][j+1], _normalBoard[i][j+2]];
@@ -149,11 +176,23 @@
 
 -(void)askUser2ForSpace{
     BOOL invalid = YES;
+    
+    //Loop while player 2 selects a space that is already occupied
     while (invalid) {
-        NSLog(@"Player 2, please enter the space you'd like to occupy.");
-        char space[256];
-        scanf("%s", space);
-        spaceChosen = [NSString stringWithCString:space encoding:NSUTF8StringEncoding];
+        
+        //If playing vs human, ask for player 2's input on space
+        if ([playerChoice isEqualToString:@"A"] || [playerChoice isEqualToString:@"a"]) {
+            NSLog(@"Player 2, please enter the space you'd like to occupy.");
+            char space[256];
+            scanf("%s", space);
+            spaceChosen = [NSString stringWithCString:space encoding:NSUTF8StringEncoding];
+        }
+        
+        //If playing vs cpu, randomly generate a space
+        if ([playerChoice isEqualToString:@"B"] || [playerChoice isEqualToString:@"b"]){
+            randomSelection = (arc4random() % 8) +1;
+            spaceChosen = [NSString stringWithFormat:@"%i", randomSelection];
+        }
         
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++) {
@@ -163,6 +202,7 @@
             }
         }
     }
+    
     [player2 setSpace:spaceChosen];
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++) {
@@ -177,6 +217,11 @@
         }
     }
     
+    if ([playerChoice isEqualToString:@"B"] || [playerChoice isEqualToString:@"b"]) {
+        NSLog(@"\n\nThe computer is selecting its space.\n\n");
+        sleep(1);
+    }
+    
     for (int i = 0; i < 3 ; i++) {
         int j = 0;
         NSString *rowColumn = [NSString stringWithFormat:@"%@ | %@ | %@", _normalBoard[i][j], _normalBoard[i][j+1], _normalBoard[i][j+2]];
@@ -185,7 +230,7 @@
             NSLog(@"---------");
         }
     }
-
+    
 }
 
 -(void)displayBlankBoard{
@@ -209,7 +254,7 @@
 -(BOOL)result{
     
     for (int i = 0; i < 3; i++) {
-    
+        
         if ((_normalBoard[i][0] == _normalBoard[i][1]) && (_normalBoard[i][0] == _normalBoard[i][2])) {
             
             return YES;
@@ -229,29 +274,19 @@
         return YES;
     }
     
-    
-//    for (int i = 0; i < 3; i++) {
-//        
-//        for (int j = 0; j < 3; j++) {
-//            if ([_normalBoard[i][j] isEqualToString:@"X"] || [_normalBoard[i][j] isEqualToString:@"O"]) {
-//                NSLog(@"Tie game!");
-//                      return YES;
-//            }
-//        }
-//    }
     return NO;
 }
 
 -(BOOL)checkIfGameIsOver{
-        for (int i = 0; i < 3; i++) {
-    
-            for (int j = 0; j < 3; j++) {
-                if (!([_normalBoard[i][j] isEqualToString:@"X"] || [_normalBoard[i][j] isEqualToString:@"O"])) {
-            
-                          return NO;
-                }
+    for (int i = 0; i < 3; i++) {
+        
+        for (int j = 0; j < 3; j++) {
+            if (!([_normalBoard[i][j] isEqualToString:@"X"] || [_normalBoard[i][j] isEqualToString:@"O"])) {
+                
+                return NO;
             }
         }
+    }
     return YES;
 }
 
