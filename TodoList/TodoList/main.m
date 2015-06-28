c0cfa501d5dbe48a17281a2da9617b60d8fc3803
Brian <brianblanco1@gmail.comm>
2015-06-28 12:33:16 -0400

    added while loop

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,65 +8,113 @@
 // delete this add
 #import <Foundation/Foundation.h>
 
-@interface toDoList: NSObject
-
-
-
--(void) enterUserList: (NSMutableArray * )userList;
--(NSArray *)userList;
--(void) manageList: (NSArray *)manList;
--(NSArray *)manList;
--(void) representList: (NSArray *)repList;
--(NSArray *)repList;
--(void) representItem: (NSString *)repItem;
--(NSString *)repItem;
-
-@end
-
-@implementation toDoList {
-    NSMutableArray *_userList;
-    NSArray *_manList;
-    NSArray *_repList;
-    NSString *_repItem;
-    
-}
-
--(void) enterUserList: (NSMutableArray *)userList {
-    _userList = userList;
-}
-
--(void) manageList:(NSArray *)manList {
-    _manList = manList;
-}
-
--(void) representList: (NSArray *)repList {
-    _repList = repList;
-}
-
--(void) representItem: (NSString *)repItem {
-    _repItem = repItem;
-}
-
-@end
+//@interface toDoList: NSObject
+//
+//
+//
+//-(void) enterUserList: (NSMutableArray * )userList;
+//-(NSArray *)userList;
+//-(void) manageList: (NSArray *)manList;
+//-(NSArray *)manList;
+//-(void) representList: (NSArray *)repList;
+//-(NSArray *)repList;
+//-(void) representItem: (NSString *)repItem;
+//-(NSString *)repItem;
+//
+//@end
+//
+//@implementation toDoList {
+//    NSMutableArray *_userList;
+//    NSArray *_manList;
+//    NSArray *_repList;
+//    NSString *_repItem;
+//    
+//}
+//
+//-(void) enterUserList: (NSMutableArray *)userList {
+//    _userList = userList;
+//}
+//
+//-(void) manageList:(NSArray *)manList {
+//    _manList = manList;
+//}
+//
+//-(void) representList: (NSArray *)repList {
+//    _repList = repList;
+//}
+//
+//-(void) representItem: (NSString *)repItem {
+//    _repItem = repItem;
+//}
+//
+//@end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
     printf("To-Do List App\n\n\n");
     
-        char addList, deleteList, editItem, markDone, priority, active,  createList;
-        int opt1, opt2, opt3, opt4, opt5, opt6;
-      
-        printf("\na) Add Item\n\nd) Delete Item\n\ne) Edit Item\n\nm) Mark Item Done\n\ns) Show Active List\n");
-        scanf("%s", &createList);
-        for (int options = 0; options <=6; options++) {
-            scanf("%d", &opt1);
-        
-        
-        
+//        char addList, deleteList, editItem, markDone, priority, active,  createList;
         
       
+        printf("1) Add Item\n\n2) Delete Item\n\n3) Edit Item\n\n4) Mark Item Done\n\n5) Show Active List\n");
+        
+        char opt;
+        int tasks = 0;
+        
+        while (tasks <=1000)
+        {
+            tasks++;
+            
+            scanf("%c", &opt);
+            fpurge(stdin);
+            
+            if (opt == 'a') {
+                
+                NSLog(@"What would you like to add?\n", opt);
+                
+            } else if (opt == 'b') {
+                
+                NSLog(@"Delete which task?", opt);
+                
+            } else if (opt == 'c') {
+                
+                NSLog(@"What would you like to edit?", opt);
+                
+            } else if (opt == 'd') {
+                
+                NSLog(@"blank is done", opt);
+                
+            } else if (opt == 'e') {
+                
+                NSLog(@"This is your current list: ", opt);
+            }
+//        for (int tasks = 0; tasks <=4 ; tasks++) {
+//
+//            scanf("%d", &opt);
+//            fpurge(stdin);
+//
+//            if (opt == 1) {
+//
+//                NSLog(@"What would you like to add?\n", opt);
+//                
+//            } else if (opt == 2) {
+//            
+//                NSLog(@"Delete which task?", opt);
+//                
+//            } else if (opt == 3) {
+//                
+//                NSLog(@"What would you like to edit?", opt);
+//                
+//            } else if (opt == 4) {
+//                
+//                NSLog(@"blank is done", opt);
+//                
+//            } else if (opt == 5) {
+//                
+//                NSLog(@"This is your current list: ", opt);
+//            }
+         }
     }
     return 0;
-}
 }
