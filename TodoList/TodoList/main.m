71ed681dc969255acf8cc2c6d0e0fd3e65a83612
Xiulan Shi <xiulanshi2013@gmail.com>
2015-06-28 12:51:51 -0400

    Updated the list class

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -8,17 +8,17 @@
 //
 
 #import <Foundation/Foundation.h>
+// create class declarations
+@class ToDoApp;
+@class ToDoList;
+@class ToDoItem;
 
-//// create class declarations
-//@class ToDoApp;
-//@class ToDoList;
-//@class ToDoItem;
-
-//ToDoItem class
+// *** ToDoItem class *** *** *** *** *** *** *** *** *** *** *** *** *** ***
 @interface ToDoItem : NSObject
 - (void)setItem:(NSString *)itemName;
 - (NSString *)itemName;
 - (void)addNameAndPriority;
+//- (BOOL)noMoreItems;
 @end
 
 @implementation ToDoItem {
@@ -40,63 +40,90 @@
     return _priority;
 }
 
+
 - (void)addNameAndPriority {
     NSLog(@"Enter item: ");
+    // "fgets" allows the user to input an item with more than one word ie: "cat food"
     char name[256];
-    scanf("%s", name);
+    fgets(name, 256, stdin);
     
+    NSLog(@"item added: %s", name); // we test our work above here
     
-    NSLog(@"added: %s", name);
-    
+    // we used "stringWithUTF8String" to convert char to string
     NSString *item1 = [NSString stringWithUTF8String:name];
-    NSLog(@"string test: %@", item1);
+//    NSLog(@"string test: %@", item1);
     
     [self setItem: item1];
     
+    // we set the priority level for each inputted item
     NSLog(@"Enter priority 1, 2, 3: ");
     int inputPriority;
     scanf("%d", &inputPriority);
     
     [self setPriority: inputPriority];
     
-    NSLog(@"%@, %d",item1, inputPriority);
+    // we print/test the inputted item name and priority
+    NSLog(@"priority level: %d", inputPriority);
     
 }
+// we set up a BOOL, so that the program knows when to stop or continue adding To Do List items:
+//-(BOOL)noMoreItems {
+//    NSLog(@"1 = add another item, 0 = stop adding items, my list is complete");
+//    int answer;
+//    scanf("%d", &answer);
+//    fpurge(stdin);
+//    
+//    return answer != 0;
+//}
 
 @end
 
 
 
-//ToDoApp class (we declare methods here)
-@interface ToDoApp : NSObject
-@end
-
-@implementation ToDoApp
-@end
-
-//ToDoList class
+// *** ToDoList class *** *** *** *** *** *** *** *** *** *** *** *** *** ***
 @interface ToDoList : NSObject
-//- (void)setListTitle:(NSString *)title;
-//- (NSString *)title;
-//- (void)setList:
+
+//@property (nonatomic) NSMutableArray *myListedItems;
+
+
+-(NSMutableArray *)myListedItems;
 -(void)addItem:(ToDoItem *)newItem;
 -(void)printList;
-
 @end
 
 @implementation ToDoList {
     NSMutableArray *_myListedItems;
 }
-// 6/27 note: we're not sure if this is adding items as we can't print out items. double check this! 
--(void)addItem:(ToDoItem *)newItem {
-    [_myListedItems addObject:newItem];
-    
+
+ //initialize our list of items:
+-(NSMutableArray *)myListedItems {
+    if (_myListedItems == nil) {
+        _myListedItems = [[NSMutableArray alloc] init];
+    }
+    return _myListedItems;
 }
 
+// this initializes our list of items (same as above, but better!)
+//- (instancetype)init
+//{
+//    if ((self = [super init])) {
+//        // create myListedItems
+//        _myListedItems = [[NSMutableArray alloc] init];
+//    }
+//    return self;
+//}
+
+
+-(void)addItem:(ToDoItem *)newItem {
+    [[self myListedItems] addObject:newItem];
+}
+
+
+
 -(void)printList {
-    NSLog(@"%lu", (unsigned long)[_myListedItems count]);
+//    NSLog(@"%lu", (unsigned long)[_myListedItems count]); 
     for (int i = 0; i < [_myListedItems count]; i++) {
-        NSLog(@"itemName:%@", [[_myListedItems objectAtIndex:i] itemName]);
+        NSLog(@"print test:%@", [[_myListedItems objectAtIndex:i] itemName]);
     }
    
 }
@@ -106,6 +133,14 @@
 //}
 
 
+@end
+
+// *** ToDoApp class *** *** *** *** *** *** *** *** *** *** *** *** *** ***
+
+@interface ToDoApp : NSObject
+@end
+
+@implementation ToDoApp
 @end
 
 
