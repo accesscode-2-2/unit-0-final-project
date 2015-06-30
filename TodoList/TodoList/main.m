ed0bccc851fb9cce46e1c1885f74cdfa02acbddc
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-29 23:32:20 -0400

    Hey, this looks like a solid start :)

--- TodoList/TodoList/main.m
+++ TodoList/TodoList/main.m
@@ -1,108 +1,92 @@
 //
 //  main.m
-//  TodoList
+//  Natalias_ToDo
 //
-//  Created by Michael Kavouras on 6/25/15.
-//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//  Created by Natalia Estrella on 6/28/15.
+//  Copyright (c) 2015 Natalia Estrella. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-BOOL notQuit = 1;
+@interface Item : NSObject
+
+@property (nonatomic) NSString *content;
+@property (nonatomic) int priority;
+
+@end
+
+@interface List : NSObject
+
+@property (nonatomic) NSMutableArray *items;
+@property (nonatomic) NSString *title;
+@property (nonatomic) int priority;
+
+-(void)enterItem;
+
+@end
 
 @interface Manager : NSObject
 
--(void) mainMenu;
--(void) storeList;
--(void) addItem;
--(void) deleteItem;
+@property (nonatomic) NSMutableArray *lists;
 
+-(void)start;
 
 @end
 
-@implementation Manager {
-    NSMutableArray *itemList;
-    NSString *userADDItem;
-}
+@implementation Item
 
--(void) mainMenu {
-    NSLog(@"Add/Delete/Quit items: ");
-    
-    while (notQuit) {
-        
-        NSFileHandle *standardInput = [NSFileHandle fileHandleWithStandardInput];
-        NSString *inputLine = [[[NSString alloc] initWithData:standardInput.availableData encoding:NSUTF8StringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-        inputLine = [inputLine lowercaseString];
-        
-        
-        if ([inputLine isEqualToString: @"add"]) {
-            [self addItem];
-            notQuit = 1;
-        } else if ([inputLine isEqualToString: @"delete"]) {
-            [self deleteItem];
-            notQuit = 1;
-        } else if ([inputLine isEqualToString: @"quit"]){
-            notQuit = 0;
-        } else {
-            NSLog(@"Invalid Entry");
-        }
-    }
+@end
 
-}
+@implementation List
 
--(void) storeList {
-    itemList = [[NSMutableArray alloc] init];
-}
-
--(void) addItem {
-
-    
-    BOOL addNotQuit = 1;
-    
-    while (addNotQuit) {
-        NSFileHandle *standardInput = [NSFileHandle fileHandleWithStandardInput];
-        NSString *uADDItem = [[[NSString alloc] initWithData:standardInput.availableData encoding:NSUTF8StringEncoding] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-        
-        if ([uADDItem isEqualToString:@"quit"]) {
-             addNotQuit = 0;
-        } else if ([uADDItem isEqualToString:@"delete"]) {
-            [self deleteItem];
-        } else  {
-            [itemList addObject:uADDItem];
-            NSLog(@"%@", itemList);
-        }
-    }
-
-    
-    
-    
-}
-
--(void) deleteItem {
-    NSLog(@"Which Item would you like to delete?");
-    int udi;
-    int userDeleteItem = udi+1;
-    scanf("%d",&udi);
-    fpurge(stdin);
-    [itemList removeObjectAtIndex:userDeleteItem];
-    NSLog(@"%@", itemList);
+-(void)enterItem {
 }
 
 @end
 
+@implementation Manager
 
+-(List *)createList {
+    List *newList = [[List alloc] init];
+    [self.lists addObject:newList];
+    return newList;
+    
+}
+
+-(void)printMenu {
+    printf("Choose form the following:\n");
+    printf("1. Create List\n");
+    printf("2. Print all lists\n");
+}
+
+-(int)getInput {
+    [self printMenu];
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+
+-(void)start {
+    while (true) {
+        int input = [self getInput];
+        if (input == 1) {
+            List *newList = [self createList];
+            
+        }
+    }
+    
+    
+}
+
+
+@end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-
-        Manager *listManager = [[Manager alloc] init];
-        [listManager storeList];
         
-        while (notQuit) {
-            [listManager mainMenu];
-        }
-        
+        Manager *manager = [[Manager alloc] init];
+        [manager start];
     }
     return 0;
 }
